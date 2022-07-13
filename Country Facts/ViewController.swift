532ff5d8d5c1435afa282a38d28eec59e9466149
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-12 20:10:59 -0500

    Random country fact button added to the table view.

--- Country Facts/ViewController.swift
+++ Country Facts/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         title = "Country facts"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
         performSelector(inBackground: #selector(loadCountries), with: nil)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Random fact", style: .plain, target: self, action: #selector(showRandomFact))
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -52,5 +56,26 @@ class ViewController: UITableViewController {
             [weak self] in
             self?.tableView.reloadData()
         }
+    }
+    
+    @objc func showRandomFact(){
+        let shuffledCountries = countries.shuffled()
+        let randomCountry = shuffledCountries[0]
+        let randomFact: String
+        
+        switch Int.random(in: 1...4) {
+        case 1:
+            randomFact = "\(randomCountry.name)'s capital is \(randomCountry.capital)."
+        case 2:
+            randomFact = "\(randomCountry.name)'s area is about \(randomCountry.size) km\u{00B2}."
+        case 3:
+            randomFact = "About \(randomCountry.population) people live in \(randomCountry.name)."
+        default:
+            randomFact = "\(randomCountry.name)'s currency is the \(randomCountry.currency)."
+        }
+        
+        let alertController = UIAlertController(title: "Did you know?", message: randomFact, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Interesting!", style: .default))
+        present(alertController, animated: true)
     }
 }
